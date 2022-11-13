c146b3dfacf07ad595770119c3fc107d5b14b2ce
Ryan Essel <spucti19@gmail.com>
2022-11-13 09:29:15 -0500

    Iteration 5 Complete a random beer works but i can't figure out how to make it show a new beer on each click

--- src/pages/RandomBeer.js
+++ src/pages/RandomBeer.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 
  function RandomBeer() {
+
+  const [randomBeer, setRandomBeer] = useState()
+  const [click, setClick] = useState(0)
+
+  useEffect(()=> {
+
+    axios
+    .get("https://ih-beers-api2.herokuapp.com/beers/random")
+    .then((response) => {
+      console.log(response.data)
+      setRandomBeer(response.data)
+
+    })
+    .catch((err)=> console.log(err))
+
+
+  }, [click])
+
+
+
+
   return (
     <div className='RandomBeer'>
         
         <h1>RandomBeer</h1>
+        {/* {!randomBeer && <h3>no beer found</h3>} */}
+
+
+        {randomBeer && (
+
+            <div>
+
+<div>need api call to get random beer</div>
+        {randomBeer.name}
+
+      
+        {randomBeer.name}
+{console.log(randomBeer.name)}
+
+<div>
+<img className='beerImg' src={randomBeer.image_url} alt="" srcset="" />
+</div>
+
+
+<div className='description'>
+
+{randomBeer.description}
+
+</div>
+
+
+<button onClick={()=> setClick(click + 1)}></button>
        
-       <div>need api call to get random beer</div>
-        
+     
+
+            </div>
+
+         
+
+
+        )}
         
     </div>
   )
